0493ae323cd1b19464cbd0493f3eb13d2cd31e35
Anjum269 <01fe24bca044.kletech@gmail.com>
2026-01-17 12:57:13 +0530

    Handle division by zero in calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import sys
+
 if len(sys.argv) == 3:
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
@@ -6,8 +7,11 @@ if len(sys.argv) == 3:
     print("Addition of two numbers:", num1 + num2)
     print("Subtraction of two numbers:", num1 - num2)
     print("Multiplication of two numbers:", num1 * num2)
-    print("Division of two numbers:", num1 / num2)
- 
+
+    if num2 != 0:
+        print("Division of two numbers:", num1 / num2)
+    else:
+        print("Division not possible (division by zero)")
 else:
     print("Invalid input")
     print("Usage: python calculator.py <num1> <num2>")
