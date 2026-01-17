852fbe282626ce7921c5ab90a293f03e1164d84e
Anjum269 <01fe24bca044.kletech@gmail.com>
2026-01-17 12:59:37 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,17 +1,12 @@
 import sys
-
-if len(sys.argv) == 2:
+if len(sys.argv) == 4:
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
 
     print("Addition of two numbers:", num1 + num2)
     print("Subtraction of two numbers:", num1 - num2)
     print("Multiplication of two numbers:", num1 * num2)
-
-    if num2 != 0:
-        print("Division of two numbers:", num1 / num2)
-    else:
-        print("Division not possible (division by zero)")
+    print("Division of two numbers:", num1 / num2)
 else:
     print("Invalid input")
  
