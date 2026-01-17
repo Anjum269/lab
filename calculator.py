d5e0c94c4d00c99e7af3ec43782b2fbb4ae558e8
Anjum269 <01fe24bca044.kletech@gmail.com>
2026-01-17 12:49:00 +0530

    Refactor variable assignments in calculator.py
    Formatted variable assignments for consistency.

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 import sys
 
 if len(sys.argv) != 3:
-num1= int(sys.argv[1])
-num2= int(sys.argv[2])
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
 
 print("Addition:", num1+num2)
 print("Subtraction:", num1 - num2)
