67ea8fdd9cb16cb0773b9842127fe867d0ec671a
Anjum269 <01fe24bca044.kletech@gmail.com>
2026-01-17 12:56:26 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,12 +1,13 @@
 import sys
-if len(sys.argv)==4:
+if len(sys.argv) == 3:
     num1 = int(sys.argv[1])
     num2 = int(sys.argv[2])
+
     print("Addition of two numbers:", num1 + num2)
     print("Subtraction of two numbers:", num1 - num2)
-    print("Multplication of two numbers:", num1 * num2)
-    print("Division of two numbers:",  num1 / num2)
+    print("Multiplication of two numbers:", num1 * num2)
+    print("Division of two numbers:", num1 / num2)
+ 
 else:
-    num1 = 2
-    num2 = 4
-    print("Invalid try again")
+    print("Invalid input")
+    print("Usage: python calculator.py <num1> <num2>")
